032f98694d274ff8e610041b2beef8cede504bf5
Aryaman Raj Tiwari <122008728+itsaryantiwari@users.noreply.github.com>
2023-06-06 10:20:58 +0530

    Update 2.01

---
+++ App.js
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+//import Collection from "./pages/collection/Collection";
+import Home from "./pages/home/Home";
+import ProductDetail from "./pages/productDetail/ProductDetail";
+import Product from "./components/product/Product";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCategories } from "./redux/categorySlice";
+import Collection from "./pages/collection/Collection";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:categoryId?" element={<Collection />} />
+          <Route path="/products/:productId?" element={<ProductDetail />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
